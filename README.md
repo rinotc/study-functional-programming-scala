1ffbb8706870bd8f08f95790ff9a9f2f7750b324
千葉俊典 <toshinori.chiba.p@gmail.com>
2020-05-24 00:57:46 +0900

    List

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Scala 関数型プログラミング学習
 
 基本的に Scala Worksheet に記述しています。
+
+参考
+
+- https://github.com/fpinscala/fpinscala/wiki
---
+++ src/chapter3/datastructures/List.scala
@@ -0,0 +1,31 @@
+package chapter3.datastructures
+
+// Scala標準ライブラリで定義されているListの少し単純化したもの
+
+// 型パラメータの + 記号は共変(covariant)であることを示す. Dog が Animal の部分型であれば, List[Dog] は List[Animal] の部分型.
+sealed trait List[+A] // データ型の定義. Aという型でパラメータ化されたListデータ型. 一般的にデータ型は trait を使って定義する. sealed はこのトレイトの実装が全てこのファイルで宣言されなければならないことを示す.
+// データコンストラクタ. case 句を使って定義されている.
+case object Nil extends List[Nothing] // 空のリストを表すデータコンストラクタ. Nil オブジェクトは空のリストを定義する.
+case class Cons[+A](head: A, tail: List[A]) extends List[A] // 空でないリストを表すデータコンストラクタ
+
+// ※コンパニオンオブジェクト
+// 同じファイルで同じネストレベルにあるクラスと同名のオブジェクトを定義したとき, そのオブジェクトを指して,
+// 「そのクラスのコンパニオンオブジェクトである」という.
+// オブジェクトのファクトリとして使われたり, 暗黙パラメータのデフォルトのインスタンスを格納するのに使われたりする.
+
+object List { // List のコンパニオンオブジェクト.
+  def sum(ints: List[Int]): Int = ints match { // 整数のリストを足し合わせる関数
+    case Nil => 0 // 空のリストの合計は0
+    case Cons(x, xs) => x + sum(xs) // x から始まるリストの合計は, xにリストの残りの合計を足したものになる.
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+}
